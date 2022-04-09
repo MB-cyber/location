ec3c6d7e9eb7c5045893ebf530d1c50733afaad4
MB-cyber <mouhssine.bentabet@gmail.com>
2022-04-09 11:32:21 +0000

    cm2

--- app/src/main/java/com/mouhssinebentabet/location/MainActivity.java
+++ app/src/main/java/com/mouhssinebentabet/location/MainActivity.java
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private GpsTracker gpsTracker;
     private TextView tvLatitude,tvLongitude;
+    private Button bmaps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         tvLatitude = (TextView)findViewById(R.id.latitude);
         tvLongitude = (TextView)findViewById(R.id.longitude);
+        bmaps=findViewById(R.id.maps);
 
         try {
             if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
@@ -28,7 +32,20 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e){
             e.printStackTrace();
         }
+
+        bmaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+
+
+                startActivity(intent);
+
+            }
+        });
     }
+
+
 
     public void getLocation(View view){
         gpsTracker = new GpsTracker(MainActivity.this);
